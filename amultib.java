e0ba61e102189be34c8496c652fcd21a871ac80d
LDH9219 <ehgus1595@gmail.com>
2022-08-03 05:29:55 +0900

    Input_output

---
+++ amultib.java
@@ -0,0 +1,25 @@
+/*
+ * 10998
+ * 문제 : 두 정수 A와 B를 입력받은 다음
+ * A * B를 출력하는 프로그램을 작성하시오.
+ * 입력 : 첫째 줄에 A와 B가 주어진다.(0<A,B<10)
+ * 첫째 줄에 A * B를 출력한다.
+ * 
+ * 두 정수를 입력받기 위해 Scanner를 import
+ * Scanner 선언
+ * 두 정수 입력
+ * Scanner 폐쇄
+ * A * B 출력
+ */
+
+import java.util.Scanner;
+
+public class amultib {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        System.out.println(a*b);
+    }
+}
